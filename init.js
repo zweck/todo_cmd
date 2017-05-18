4c8779458dbe20c353db149cf5d6314a703eaa7d
Phil Hauser <phil@zweck.co>
2017-05-18 19:24:16 +0100

    Add git support to todo and reorder help

--- init.js
+++ init.js
@@ -2,11 +2,12 @@ const chalk = require('chalk');
 const fs = require('fs');
 const path = require('path');
 const tilde = require('tilde-expansion');
+const shell = require('shelljs');
 const appRootDir = require('app-root-dir').get();
 const setConfigProp = require('./setConfigProp');
 const newTodoMonth = require('./newTodoMonth');
 
-const init = function({ dir, withGit }){
+const init = function({ dir, withGit=false }){
   tilde(dir, (expandedDir) => {
     if (!fs.existsSync(expandedDir)) return console.log(chalk.red('Directory for workbook doesnt exist'));
     console.log(chalk.green(`Initializing new todo workbook in ${dir}`));
@@ -15,8 +16,15 @@ const init = function({ dir, withGit }){
 
     if (!fs.existsSync( todoRoot )) fs.mkdirSync( todoRoot );
     writeConfig( configPath, todoRoot );
-    setConfigProp({ todoRoot });
+    setConfigProp({ todoRoot, withGit });
     newTodoMonth( todoRoot );
+    if (withGit) {
+      console.log(chalk.blue('Initialising with git'));
+      shell.cd(expandedDir);
+      shell.exec('git init');
+      shell.exec('git add --all');
+      shell.exec('git commit -m "Initial Commit"');
+    }
   });
 };
 
